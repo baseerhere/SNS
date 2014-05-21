35baa741ba13eadd83db240fb822d3a1ebd7ab45
baseerhere <baseerhere@hotmail.com>
2014-05-21 19:27:00 +0530

    added user basic info

---
+++ SNS/Models/OS.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SNS.Models
+{
+    public class OS
+    {
+    }
+}
--- SNS/Models/User.cs
+++ SNS/Models/User.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +9,23 @@ namespace SNS.Models
 {
     public class User
     {
-        
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string NickName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int Gender { get; set; }
+
+        public UserLoginHistory[] UserLoginHistory { get; set; }
     }
 }
